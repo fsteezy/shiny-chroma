7fa4be047adca3a576872158e66ed94378a1c056
Fabian Gomez <fabian.gomez@student.csulb.edu>
2023-08-24 11:07:47 -0700

    Tried to create a code to create a simple Shiny app that allows users to upload embedding data and visualize it using the chRoma package

--- ShinyAppChromaLastRun.R
+++ ShinyAppChromaLastRun.R
@@ -1,3 +1,29 @@
 library(shiny)
 library(chRoma)
 
+ui <- fluidPage(
+  titlePanel("chRoma Embedding Analysis"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("embeddingFile", "Upload Embedding Data", accept = ".csv"),
+      actionButton("analyzeButton", "Analyze Embedding")
+    ),
+    mainPanel(
+      plotOutput("embeddingPlot")
+    )
+  )
+)
+
+server <- function(input, output) {
+  observeEvent(input.analyzeButton, {
+    req(input.embeddingFile)
+    embeddings <- read.csv(input.embeddingFile$datapath)
+    # Assuming your data has columns like x, y, label
+    output$embeddingPlot <- renderPlot({
+      chRoma::chRoma_plot(embeddings, x = "x", y = "y", label = "label")
+    })
+  })
+}
+
+shinyApp(ui, server)
+
